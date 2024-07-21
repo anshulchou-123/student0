80039e122a3d410cf582598f45f652cc3b1c96f4
Anshul Chouhan <anshulchouhan130@gmail.com>
2024-07-21 10:36:07 +0000

    Finished my first CS 162 assignment

--- hw-intro/recurse.c
+++ hw-intro/recurse.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int recur(int i) {
     printf("i is %i. Address of i is %p\n", i, &i);
+    char* p = malloc(2);
+    printf("Address of dynamic memory is %p\n", (void*)p);
 
     if (i > 0) {
         return recur(i - 1);
     }
 
+    free(p);
     return 0;
 }
--- hw-intro/words/word_count.c
+++ hw-intro/words/word_count.c
@@ -24,15 +24,15 @@ Mutators take a reference to a list as first arg.
 
 /* Basic utilities */
 
-char *new_string(char *str) {
-  char *new_str = (char *) malloc(strlen(str) + 1);
+char* new_string(char* str) {
+  char* new_str = (char*)malloc(strlen(str) + 1);
   if (new_str == NULL) {
     return NULL;
   }
   return strcpy(new_str, str);
 }
 
-int init_words(WordCount **wclist) {
+int init_words(WordCount** wclist) {
   /* Initialize word count.
      Returns 0 if no errors are encountered
      in the body of this function; 1 otherwise.
@@ -41,32 +41,70 @@ int init_words(WordCount **wclist) {
   return 0;
 }
 
-ssize_t len_words(WordCount *wchead) {
+ssize_t len_words(WordCount* wchead) {
   /* Return -1 if any errors are
      encountered in the body of
      this function.
   */
-    size_t len = 0;
-    return len;
+  size_t len = 0;
+  WordCount* whead = wchead;
+  while (whead != NULL) {
+    len++;
+    whead = whead->next;
+  }
+
+  return len;
 }
 
-WordCount *find_word(WordCount *wchead, char *word) {
+WordCount* find_word(WordCount* wchead, char* word) {
   /* Return count for word, if it exists */
-  WordCount *wc = NULL;
+  WordCount* wc = NULL;
+  WordCount* whead = wchead;
+
+  while (whead != NULL) {
+    if (strcmp(whead->word, word) == 0) {
+      wc = whead;
+      break;
+    }
+    whead = whead->next;
+  }
   return wc;
 }
 
-int add_word(WordCount **wclist, char *word) {
+int add_word(WordCount** wclist, char* word) {
   /* If word is present in word_counts list, increment the count.
      Otherwise insert with count 1.
      Returns 0 if no errors are encountered in the body of this function; 1 otherwise.
   */
- return 0;
+
+ if(wclist == NULL)
+ {
+  return 1;
+ }
+  if(*wclist != NULL)printf("Head of the string %s\n", (*wclist)->word);
+  WordCount* whead = *wclist;
+  while (whead != NULL) {
+    printf("String %s and %s are equal\n", whead->word, word);
+    if (strcmp(whead->word, word) == 0) {
+      whead->count++;
+      return 0;
+    }
+    whead = whead->next;
+  }
+
+  WordCount* wc = (WordCount*)malloc(sizeof(WordCount));
+  wc->word = word;
+  wc->count = 1;
+
+  wc->next = *wclist;
+  *wclist = wc;
+  printf("Word added to list is %s\n", wc->word);
+  return 0;
 }
 
-void fprint_words(WordCount *wchead, FILE *ofile) {
+void fprint_words(WordCount* wchead, FILE* ofile) {
   /* print word counts to a file */
-  WordCount *wc;
+  WordCount* wc;
   for (wc = wchead; wc; wc = wc->next) {
     fprintf(ofile, "%i\t%s\n", wc->count, wc->word);
   }
